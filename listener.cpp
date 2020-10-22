e178c56cb0b92385bee09084cab99c782f36aa24
Ricardo <ruizr274@gmail.com>
2020-10-22 12:07:02 -0400

    listiner code/not done

---
+++ listener.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include "listener.h"
+
+using namespace std;
+void listener::print(){
+	printf("testing\n");
+	printf("asd");
+}
+
---
+++ listener.h
@@ -0,0 +1,10 @@
+
+#ifndef LISTENER_H 
+#define LISTENER_H 
+
+class listener{
+    public: 
+    void print();
+};
+
+#endif
